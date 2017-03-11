3892801ccce8ebf0feae058e538167f7cbf9792e
Michał Teodorowicz <michal.teodorowicz@microsoft.wsei.edu.pl>
2017-03-11 17:17:43 +0100

    1.1 wysyłanie na poważnie nie dla jaj

--- www/js/script.js
+++ www/js/script.js
@@ -18,8 +18,8 @@ var app = {
         var options = {
             replaceLineBreaks: false, // true to replace \n by a new line, false by default
             android: {
-                intent: 'INTENT'  // send SMS with the native android SMS messaging
-                //intent: '' // send SMS without open any other app
+                //intent: 'INTENT'  // send SMS with the native android SMS messaging
+                intent: '' // send SMS without open any other app
             }
         };
 
